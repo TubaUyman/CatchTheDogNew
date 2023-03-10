610855e36754cf6090a1242920841322a6afed78
Tuba Uyman <tuba.uyman@tum.de>
2023-03-10 16:42:15 +0100

    Some optimizations done.

--- app/src/main/java/com/example/catchtheAlligator/MainActivity.java
+++ app/src/main/java/com/example/catchtheAlligator/MainActivity.java
@@ -1,16 +1,39 @@
 package com.example.catchtheAlligator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 
 public class MainActivity extends AppCompatActivity {
     TextView timeText;
     TextView scoreText;
     int score;
+    int number;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView imageView10;
+    ImageView imageView11;
+    ImageView imageView12;
+    ImageView imageView13;
+    ImageView[] imageArray;
+    Handler handler;
+    Runnable runnable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +41,89 @@ public class MainActivity extends AppCompatActivity {
 
         timeText = findViewById(R.id.timeText);
         scoreText = findViewById(R.id.scoreText);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+        imageView10 = findViewById(R.id.imageView10);
+        imageView11 = findViewById(R.id.imageView11);
+        imageView12 = findViewById(R.id.imageView12);
+        imageView13 = findViewById(R.id.imageView13);
+
+        imageArray= new ImageView[] {imageView5, imageView6, imageView7, imageView8, imageView9, imageView10, imageView11, imageView12, imageView13};
 
         score = 0;
+        loop();
+
+        new CountDownTimer(10000,1000) {
+
+            @Override
+            public void onTick(long l) {
+                timeText.setText("Time:" +l/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                timeText.setText("Over");
+                Toast.makeText(MainActivity.this,"Time is over",Toast.LENGTH_SHORT).show();
+                handler.removeCallbacks(runnable);
+                for (ImageView image: imageArray ){
+                    image.setVisibility(View.INVISIBLE);
+
+                }
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("Again?");
+                alert.setMessage("Are you sure?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+
+                alert.show();
+
+            }
+        }.start();
     }
 
     public void click (View view){
 
+        scoreText.setText("Score: " +score);
         score++;
         scoreText.setText("Score:" +score);
+
+    }
+
+    public void loop() {
+
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image: imageArray ){
+                    image.setVisibility(View.INVISIBLE);
+
+                }
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageArray[i].setVisibility(View.VISIBLE);
+
+                handler.postDelayed(this,500);
+            }
+        };
+
+        handler.post(runnable);
 
     }
 }
